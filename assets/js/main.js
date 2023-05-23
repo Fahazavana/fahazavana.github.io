12e602afd0ee1c98953e88bc1732f576c3bc8b30
Jean Lucien Randrianatenaina <rjlucienaina@gmail.com>
2023-05-23 17:49:11 +0100

    Improving desing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,63 @@
+function typing(elt,text) {
+    const n = text.length
+
+    // Speed controller
+    const delay = 200;
+
+    let i = 0;
+
+    setInterval(() => {
+        elt.innerHTML = text.substring(0, Math.abs(i));
+        i += 1;
+        if (i >= n) {
+            i = 0
+            elt.innerHTML =''
+            // i *= -1;
+        }
+    }, delay)
+}
+
+
 function linkState(id) {
-  let current = document.getElementsByClassName("active");
-  let newCurrent = document.getElementById(id);
-  console.log(id);
-  current[0].classList.remove("active");
-  newCurrent.classList.add("active");
+    const current = document.getElementsByClassName("active");
+    const newCurrent = document.getElementById(id);
+    console.log(id);
+    current[0].classList.remove("active");
+    newCurrent.classList.add("active");
 }
 
 function loadHTML(page, request, updatenav) {
-  fetch(page)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("mainContainer").innerHTML = data;
-    })
-    .catch((error) => console.error(error));
-  updatenav(`link-${request}`);
+    fetch(page)
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("mainContainer").innerHTML = data;
+        })
+        .catch((error) => console.error(error));
+    updatenav(`link-${request}`);
 }
 
 function getContent(request) {
-  let contentToReturn;
-  console.log(request);
-  switch (request) {
-    case "home":
-      contentToReturn = "home.html";
-      break;
-    case "cv":
-      contentToReturn = "cv.html";
-      break;
-    case "work":
-      contentToReturn = "work.html";
-      break;
-    case "contact":
-      contentToReturn = "contact.html";
-      break;
-  }
-  loadHTML(`assets/fragments/${contentToReturn}`, request, linkState);
+    let contentToReturn;
+    console.log(request);
+    switch (request) {
+        case "home":
+            contentToReturn = "home.html";
+            break;
+        case "cv":
+            contentToReturn = "cv.html";
+            break;
+        case "work":
+            contentToReturn = "work.html";
+            break;
+        case "contact":
+            contentToReturn = "contact.html";
+            break;
+    }
+    loadHTML(`assets/fragments/${contentToReturn}`, request, linkState);
 }
 
 
-window.onload=()=>{
-getContent('home')
+window.onload = () => {
+    getContent('home')
+    typing(document.getElementById("name"),'Jean Lucien Randrianatenaina')
 }
-// function getContent(request) {
-//     let xhttp;
-//     let elt = document.getElementById("mainContainer");
-//     if (request) {
-//         xhttp = new XMLHttpRequest();
-//         xhttp.onreadystatechange = function () {
-//             if (this.readyState == 4) {
-//                 console.log(this.status)
-
-//                 if (this.status == 200) {
-//                     elt.innerHTML = this.responseText;
-//                 }
-
-//                 if (this.status == 404) {
-//                     elt.innerHTML = '<h1>Page not found</h1>';
-//                 }
-
-//             }
-//         }
-//         xhttp.open("GET",`assets/fragments/${request}.html`,true)
-//         xhttp.send();
-//         return;
-//     }
-// }
