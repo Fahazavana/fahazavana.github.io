464645e485f2b2ab1a2958df3c2ae9ac4311fcb7
Jean Lucien Randrianantenaina <rjlucienaina@gmail.com>
2023-10-19 12:34:50 +0300

    Adjust UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,12 +32,12 @@ menuBtn.addEventListener('click', () => {
 
 
 // Menu
-// Get all menu items
 var menuItems = document.querySelectorAll('.menu-nav-item');
-
-// Add click event listener to each menu item
 menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener('click', function (event) {
+  menuItem.addEventListener('click', function (e) {
+    e.preventDefault()
+    let target = document.querySelector(this.firstElementChild.getAttribute('href'))
+    target.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
     menuItems.forEach(function (item) {
       item.classList.remove('active');
     });
@@ -45,17 +45,7 @@ menuItems.forEach(function (menuItem) {
   });
 });
 
-
-// Timeline
-
-const education = document.getElementById("educ");
-const timeline = document.getElementsByClassName("timeline")[0];
-
-function updateHeight() {
-  timeline.style.height = education.offsetHeight + "px";
-}
-
-
+// Arrow scrool
 function updateArrow(elt) {
   console.log(elt.classList)
   if (elt.classList.contains('bi-chevron-down')) {
@@ -65,10 +55,14 @@ function updateArrow(elt) {
     elt.classList.toggle('bi-chevron-up')
     elt.classList.toggle('bi-chevron-down')
   }
-
+}
+// Timeline
+const education = document.getElementById("educ");
+const timeline = document.getElementsByClassName("timeline")[0];
+function updateHeight() {
+  timeline.style.height = education.offsetHeight + "px";
 }
 // Collapsible
-
 let headers = document.getElementsByClassName("education expander");
 for (let i = 0; i < headers.length; i++) {
   headers[i].addEventListener("click", function () {
@@ -84,12 +78,9 @@ for (let i = 0; i < headers.length; i++) {
     }
   });
 }
-
 // Initial height update
 updateHeight();
-
-
-
+//project
 let col = document.getElementsByClassName("project-expander");
 for (let i = 0; i < col.length; i++) {
   col[i].addEventListener("click", function () {
@@ -106,18 +97,14 @@ for (let i = 0; i < col.length; i++) {
 
 //// CONTACTME handler
 
-
-
-
-
 const form = document.getElementById('contactform');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const FORM = 'https://docs.google.com/forms/d/e/1FAIpQLScQW83ZO4ig1h-0JMDqr_d7_PxbPcxcz1SL1J48jexh7-y4Dg/formResponse'
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); 
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
   if (!validateForm()) {
     return;
   }
@@ -126,23 +113,23 @@ form.addEventListener('submit', function(event) {
 
   fetch(FORM, {
     method: 'POST',
-    mode:'no-cors',
+    mode: 'no-cors',
     body: formData
   })
-  .then(response => {
-    console.log(response)
-    if (response.status === 0 || response.ok){
-      alert('Sent')
-      form.reset()
-    }else{
-      if (response.status === 403){
-        alert('Something went wrong')
+    .then(response => {
+      console.log(response)
+      if (response.status === 0 || response.ok) {
+        alert('Sent')
+        form.reset()
+      } else {
+        if (response.status === 403) {
+          alert('Something went wrong')
+        }
       }
-    }
-  })
-  .catch(error => {
-    console.log('Form submission error:', error);
-  });
+    })
+    .catch(error => {
+      console.log('Form submission error:', error);
+    });
 });
 
 function validateForm() {
@@ -178,9 +165,42 @@ function validateForm() {
 }
 
 function isValidEmail(email) {
-  // Regular expression for email validation
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
+}
+
+
+
+// Skill bar
+
+let skills = document.getElementsByClassName('skill')
+const minVal = 0,maxVal = 100;
+const minCol = '#808080', maxCol = '#FFFF00';
+console.log(skills)
+
+for (let i=0; i<skills.length;i++){
+  let level = parseInt(skills[i].dataset.level)
+  let perc = level/100
+  let bg = makeGradient(minCol,maxCol,perc) 
+  console.log(bg,perc)
+  skills[i].style.backgroundImage = `linear-gradient(to right,${minCol},${bg})`;
+  skills[i].style.width = `${level}%`
+}
+
+function makeGradient(color1,color2,percentage){
+  const color1R = parseInt(color1.substr(1, 2), 16);
+  const color1G = parseInt(color1.substr(3, 2), 16);
+  const color1B = parseInt(color1.substr(5, 2), 16);
+
+  const color2R = parseInt(color2.substr(1, 2), 16);
+  const color2G = parseInt(color2.substr(3, 2), 16);
+  const color2B = parseInt(color2.substr(5, 2), 16);
+
+  const interpolatedR = Math.round(color1R + (color2R - color1R) * percentage);
+  const interpolatedG = Math.round(color1G + (color2G - color1G) * percentage);
+  const interpolatedB = Math.round(color1B + (color2B - color1B) * percentage);
+
+  return `rgb(${interpolatedR}, ${interpolatedG}, ${interpolatedB})`;
 }
 
 
